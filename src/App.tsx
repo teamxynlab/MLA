f3262e48efe27e6b11c95a9a701f2ecfb1f55c9c
xyntechx <xyntechx@gmail.com>
2023-03-08 08:01:05 +0800

    🎉 Init app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <main className="container">
+            <p>Hello MLA</p>
+        </main>
+    )
+}
+
+export default App;
